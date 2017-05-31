d1f68c8f029afa3a05c3e94c974c2cfb26045d60
liudongming <928012536@qq.com>
2017-05-31 09:39:50 +0800

    1

---
+++ app/src/main/java/example/ldm/ltkj/surpertoastcla/MainActivity.java
@@ -0,0 +1,13 @@
+package example.ldm.ltkj.surpertoastcla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/example/ldm/ltkj/surpertoastcla/ToastUtils.java
@@ -0,0 +1,27 @@
+package example.ldm.ltkj.surpertoastcla;
+
+/**
+ * Created by dell on 2017/5/31.
+ */
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 吐司工具类
+ *
+ * @author chenjunxu
+ * @date 16/8/30
+ */
+public class ToastUtils {
+
+    /**
+     * 显示吐司
+     *
+     * @param context
+     * @param str
+     */
+    public static void showToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+}
